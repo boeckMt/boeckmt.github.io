b9c9cdcc498e37d16918606f70bd1fe3a37216fa
Mathias Boeck <bbmatdd@gmail.com>
2014-08-07 22:21:28 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+angular.module("boeckMt", [])
+	.controller("mainCtrl", function ($scope) {
+		var user = {
+			"name": "BoeckMt",
+			"gitPageUrl":"https://github.com/boeckMt"
+		};
+
+		var repos = {
+			"1":{
+				"name":"esri-application-boilerplate-viewer",
+				"desc":"Configurable web mapping viewer based on esri's application-boilerplate-js",
+				"url":"http://boeckmt.github.io/application_boilerplate_viewer/?config=config2.xml"
+			}
+		};
+		$scope.repos = repos;
+		$scope.user = user;
+	});
+
