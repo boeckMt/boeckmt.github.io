99845dc70f8aa6d4f1795d1c395d0f2bc79ff101
boeckMt <bbmatdd@gmail.com>
2019-03-07 22:35:32 +0100

    add repo timeline

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,19 +1,42 @@
 //
 
 let url = 'https://api.github.com/users/boeckmt/repos?type=sources';
+
+function compare(a, b) {
+  let _a = new Date(a.created_at).getTime(), _b = new Date(b.created_at).getTime();
+  if (_a < _b)
+    return 1;
+  if (_a > _b)
+    return -1;
+  return 0;
+}
+
 fetch(url)
-  .then((response)=> {
+  .then((response) => {
     return response.json();
   })
-  .then((myJson:any[])=> {
-      let repos = myJson.filter(item => !item.fork).map(item => item.name);
-      let inseret_container = document.getElementById('insert');
-      inseret_container.outerHTML = `${
-        repos.map((item)=>{
-            return `<p>${item}</p>`
-        }).join(" ")
+  .then((myJson: any[]) => {
+    let repos = myJson.filter(item => !item.fork).sort(compare);//.map(item => item.name);
+    let inseret_container = document.getElementById('insert');
+    inseret_container.outerHTML = `<div class="timeline-container">${
+      repos.map((item) => {
+        return `
+          <div class="timeline-block">
+          <h4 class="timeline-title"><a href="${item.clone_url}" target="_blank">${item.name}</a></h4>
+          <div class="timeline-block-content">
+            ${item.description || ''}
+          </div>
+          <div class="timeline-block-date">
+            ${item.created_at.split('T')[0]}
+          </div>
+        </div>`
+      }).join(" ")
       }
       
-      `;
+      </div>`;
     console.log(repos);
   });
+
+
+
+
